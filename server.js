f8bda731ba0768de0eb59c1eabb5bd5192fb47bf
Meet-Maheshwari <meetmaheshwari2107@gmail.com>
2025-04-04 23:06:45 +0530

    Setting cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors"
+import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoutes.js";
@@ -9,20 +9,22 @@ import userRouter from "./routes/userRoutes.js";
 const app = express();
 const port = process.env.PORT || 4000;
 
+const allowOrigins = ["https://auth-backend-ptwi.onrender.com"];
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true}));
-app.use(express.urlencoded( {extended: true}));
+app.use(cors({ origin: allowOrigins, credentials: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => console.log(`Server started on PORT ${port}`));
 
 async function main() {
-    await mongoose.connect(`${process.env.MONGO_URL}/mern-auth`);
+  await mongoose.connect(`${process.env.MONGO_URL}/mern-auth`);
 }
 
 main()
-.then(() => console.log("Connected to database"))
-.catch((err) => console.log(err));
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log(err));
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
