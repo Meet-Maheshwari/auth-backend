664ee53978a209969f5553e7a143f6ef4e52be4b
Meet-Maheshwari <meetmaheshwari2107@gmail.com>
2025-04-04 21:50:45 +0530

    Added all backend files

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors"
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}));
+app.use(express.urlencoded( {extended: true}));
+
+app.listen(port, () => console.log(`Server started on PORT ${port}`));
+
+async function main() {
+    await mongoose.connect(`${process.env.MONGO_URL}/mern-auth`);
+}
+
+main()
+.then(() => console.log("Connected to database"))
+.catch((err) => console.log(err));
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
